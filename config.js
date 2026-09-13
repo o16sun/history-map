83b368da1bf98c57eff7550dc0eb3df922f0aa28
o16sun <beaute8@icloud.com>
2026-09-13 10:03:41 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  // GoogleフォームのURL
-  formUrl: "https://forms.gle/cqTQUZa9BE3dYccM6",
+  // GoogleフォームのURL（※必ず半角の double quotes "" で囲んでください）
+  formUrl: "https://docs.google.com/forms/d/e/1FAIpQLS...",
   
   // サイトタイトル
   siteTitle: "歴史風景マップ"

5144a9aa0cd82bce31d49455729cf186c104c432
o16sun <beaute8@icloud.com>
2026-09-13 10:26:27 +0900

    Update Google Form URL in config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 const CONFIG = {
-  // GoogleフォームのURL（※必ず半角の double quotes "" で囲んでください）
-  formUrl: "https://docs.google.com/forms/d/e/1FAIpQLS...",
-  
-  // サイトタイトル
+  formUrl: "https://forms.google.com",
   siteTitle: "歴史風景マップ"
 };
